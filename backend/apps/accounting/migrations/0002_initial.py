4a4de35b0408ec7cef3bae1da41bebcfb710c2c8
Jitendra Khadka <jitendrakhadka4@gmail.com>
2026-04-30 23:07:44 +0900

    fix: correct initial flags in migrations

--- backend/apps/accounting/migrations/0002_initial.py
+++ backend/apps/accounting/migrations/0002_initial.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 
 class Migration(migrations.Migration):
 
-    initial = True
+    initial = False
 
     dependencies = [
         ('core', '0001_initial'),
--- backend/apps/accounting/migrations/0003_initial.py
+++ backend/apps/accounting/migrations/0003_initial.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 
 class Migration(migrations.Migration):
 
-    initial = True
+    initial = False
 
     dependencies = [
         ('accounting', '0002_initial'),
